1b3fa60c41c40340e0287312d96efe82027e15f8
Artur Gasparyan <gasparyanartur99@gmail.com>
2023-02-03 15:08:15 +0100

    Finish type hinting for parameters

--- src/channel_mmWave_parameters.py
+++ src/channel_mmWave_parameters.py
@@ -18,7 +18,7 @@ Initialize default channel parameters, including several parts.
 import numpy as np
 from dataclasses import dataclass
 from enum import Enum, auto, unique
-from utils import db2pow, npa
+from .utils import db2pow, npa
 
 @unique
 class SynType(Enum):
@@ -62,22 +62,22 @@ class RadiationType(Enum):
     Cos2 = auto()
 
 
-@dataclass
+@dataclass(init=True)
 class ChannelmmWaveParameters:
     # Model parameters
-    syn_type = SynType.Asyn
-    link_type = LinkType.Uplink
+    syn_type: SynType = SynType.Asyn
+    link_type: LinkType = LinkType.Uplink
 
     # Signal 
-    beam_type = BeamType.Random
-    ris_profile_type = RisProfileType.Random
+    beam_type: BeamType = BeamType.Random
+    ris_profile_type: RisProfileType = RisProfileType.Random
     beamforming_angle_std: float = 0        # beamforming angle variation
-    array_type = ArrayType.Analog
+    array_type: ArrayType = ArrayType.Analog
     radio_directional: float = 1        # ratio of directional beams
 
-    UE_radiation = RadiationType.Omni
-    RIS_radiation = RadiationType.Omni
-    BS_radiation = RadiationType.Omni
+    UE_radiation: RadiationType = RadiationType.Omni
+    RIS_radiation: RadiationType = RadiationType.Omni
+    BS_radiation: RadiationType = RadiationType.Omni
 
     # Signal parameters
     c: float = 3E8      # speed of light 
@@ -93,18 +93,18 @@ class ChannelmmWaveParameters:
     # Global position: 3x1 vector (2D by settings P(3) as zero)
 
     # BS: Base Station
-    PB = npa([0, 0, 0]).T
-    OB = npa([90, 0, 0]).T
+    PB: np.ndarray = np.array([0, 0, 0]).T
+    OB: np.ndarray = np.array([90, 0, 0]).T
 
     # UE: User Equipment
-    PU = npa(5, 2, 0).T
-    OU = npa(0, 0, 0).T
-    VU = npa(0, 0, 0).T
+    PU: np.ndarray = np.array([5, 2, 0]).T
+    OU: np.ndarray = np.array([0, 0, 0]).T
+    VU: np.ndarray = np.array([0, 0, 0]).T
 
     # RIS: Reconfigurable Intelligent Surfaces
-    PR = npa([])
-    OR = npa([0, 0, 0]).T
-    VR = npa([0, 0, 0]).T
+    PR: np.ndarray = np.array([])
+    OR: np.ndarray = np.array([0, 0, 0]).T
+    VR: np.ndarray = np.array([0, 0, 0]).T
 
     # Infrastructure Parameters
     # Number of RFCs at BS and UE
@@ -112,15 +112,15 @@ class ChannelmmWaveParameters:
     MU: int = 1
 
     # Array Dimensions
-    NB_dim = npa([4, 4]).T
-    NR_dim = npa([10, 10]).T
-    NU_dim = npa([4, 4]).T
+    NB_dim: np.ndarray = np.array([4, 4]).T
+    NR_dim: np.ndarray = np.array([10, 10]).T
+    NU_dim: np.ndarray = np.array([4, 4]).T
 
     # Number of elements at BS/RIS/UE
     # Number of antennas for conventional array 
-    NB: int = np.prod(NB_dim, 1)    # Number of BS elements
-    NR: int = np.prod(NR_dim, 1)    # Number of RIS elements
-    NU: int = np.prod(NU_dim, 1)    # Number of UE elements
+    NB: int = np.prod(NB_dim, 0)    # Number of BS elements
+    NR: int = np.prod(NR_dim, 0)    # Number of RIS elements
+    NU: int = np.prod(NU_dim, 0)    # Number of UE elements
 
     # Environment Parameters
     operationBW: float = 400E6      # Operation bandwidth for Thermal noise
@@ -130,6 +130,6 @@ class ChannelmmWaveParameters:
     Pn_dBm: float = 10 * np.log10(Pn)       # Thermal noise in dB
     sigma_in: float = np.sqrt(Pn)           # Johnson-Nyquist noise: sigma^2 = N_0
     noise_figure: float = 10                # Noise figure 3dB
-    sigma: float = np.sqrt(np.pow(10, noise_figure/10)) * sigma_in      # Output noise level
+    sigma: float = np.sqrt(np.power(10, noise_figure/10)) * sigma_in      # Output noise level
 
 
