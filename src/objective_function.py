fde0f7659c053df880179d0f88f91d4e2027b908
gasparyanartur <gasparyanartur99@gmail.com>
2023-03-11 15:57:35 +0100

    feat: Implement get_slots

--- src/objective_function.py
+++ src/objective_function.py
@@ -1,64 +1,137 @@
+""" objective_function.py
+
+Author: Artur Gasparyan
+gasparyanartur99@gmail.com
+
+Author:  Ruiqi Qiu
+qiuruiqi1991@gmail.com 
+
+Contains functionality for generating objective scores of positioning in environment.
+"""
+
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from src.channel_mmWave_parameters import ChannelmmWaveParameters
 from typing import Tuple
 
-def get_slots_from_side(side: np.ndarray, n: int) -> np.ndarray:
+
+def get_slots_from_side(side: np.ndarray, offset: np.ndarray, n: int) -> np.ndarray:
     """ Split side to n evenly spaced slots. 
 
     Args:
-        side <N>: The discrete positions of the side.
+        side <N, 2>: The discrete positions of the side.
+        offset: <1, 2>: The offset of the slots to the side.
         n: The number of slots to divide into.
 
     Returns:
         slots <n>: The positions of the slots. 
     """
-    if len(side.shape) > 1:
-        raise ValueError(f"Side must be flat. Received side with dimensions {side.shape}.")
+    if len(side.shape) != 2:
+        raise ValueError(
+            f"Side must be 2D. Received side with dimensions {side.shape}.")
 
     if n < 1:
-        raise ValueError(f"Cannot divide side of length {len(side)} into {n} slots.")
+        raise ValueError(
+            f"Cannot divide side of length {len(side)} into {n} slots.")
 
     N = len(side)
     gap = N/(n+1)
     idx = (np.arange(1, n+1) * gap).astype(int)
-    return side[idx]
+    slots = side[idx] + offset
+    return slots
+
+
+def get_slots(
+    grid_x: np.ndarray,
+    grid_y: np.ndarray,
+    n_slots: Tuple[int, int, int, int],
+    slot_dists: Tuple[float, float, float, float]
+) -> Tuple[np.ndarray]:
+    """ Generate slots around a grid at a given distance.
+
+    Args:
+        grid_x <Nx>: The x-positions of the grid.
+        grid_y <Ny>: The y-positions of the grid.
+        n_slots (left, right, bot, top): The number of slots at a given side.
+        slot_dists (left, right, bot, top): The distance between the slots and the sides (always positive).
+
+    Returns:
+        slots (left, right, bot, top) <N, 2>: The two-dimensional position of each slot for each side.
+    """
+    n_x = len(grid_x)
+    n_y = len(grid_y)
+
+    ys = (
+        grid_y,
+        grid_y,
+        np.repeat(grid_y[0], n_x),
+        np.repeat(grid_y[-1], n_x)
+    )
+    xs = (
+        np.repeat(grid_x[0], n_y),
+        np.repeat(grid_x[-1], n_y),
+        grid_x,
+        grid_x
+    )
+
+    sd_l, sd_r, sd_b, sd_t = slot_dists
+    offsets = np.array([(0, -sd_l), (0, sd_r), (-sd_b, 0), (sd_t, 0)])
+
+    slots = []
+    for y, x, offset, n in zip(ys, xs, offsets, n_slots):
+        p = np.vstack((y, x)).T
+        slot = get_slots_from_side(p, offset, n)
+        slots.append(slot)
+
+    return tuple(slots)
 
 
 class ObjectiveFunction:
     def __init__(self,
-                 resolution: Tuple[float, float] = (20, 10),
-                 x_range: Tuple[float] = (-5, 5), 
-                 y_range: Tuple[float] = (0, 5),
-                 n_pos_hori: int = 8,
-                 n_pos_vert: int = 4
+                 grid_range: Tuple[float, float, float, float] = (-5, 5, 0, 5),
+                 grid_step: Tuple[float, float] = (0.5, 0.5),
+                 n_slots: Tuple[int, int, int, int] = (8, 4, 8, 4),
+                 slot_dists: Tuple[float, float, float,
+                                   float] = (0.1, 0.1, 0.1, 0.1),
                  ) -> None:
-        x_res, y_res = resolution
-        x_min, x_max = x_range
-        y_min, y_max = y_range
+        """ Defines an objective function for a given configuration of the environment.
 
+        Args:
+            grid_range (left, right, bot, top): Which coordinates the grid spans.
+            grid_step (x, y): How small steps the grid is dividied into.
+            n_slots (left, right, bot, top): How many possible locations the equipment can be placed at.
+            slot_dists (left, right, bot, top): The distance each slot has to the corresponding side.
+        """
+        x_min, x_max, y_min, y_max = grid_range
+        x_step, y_step = grid_step
 
+        grid_x = np.arange(x_min, x_max+x_step, x_step)
+        grid_y = np.arange(y_min, y_max+y_step, y_step)
+
+        self.slots = get_slots(grid_x, grid_y, n_slots, slot_dists)
 
     def get_score(self, debug=False):
         c = ChannelmmWaveParameters(
-            PB = np.array([0, -0.01, 0]).reshape(-1, 1),
-            OB = np.array([45, 0, 0]).reshape(-1, 1),
-            NB_dim = np.array([1, 1]).reshape(-1, 1),
-            PR = np.array([[-5.01, 2, 0.5], [-1, 5.01, 0.5], [5.01, 2.5, 0.5]]).T,
-            OR = np.array([[0, 0, 0], [-90, 0, 0], [180, 0, 0]]).T,
-            NR_dim = np.array([[10, 10], [10, 10], [10, 10]]).T,
-            PU = np.array([-2, 3, -1.5]).reshape(-1, 1),
-            OU = np.array([0, 0, 0]).reshape(-1, 1),
-            NU_dim = np.array([1, 1]).reshape(-1, 1),
-            K = 64,
-            G = 10,
+            PB=np.array([0, -0.01, 0]).reshape(-1, 1),
+            OB=np.array([45, 0, 0]).reshape(-1, 1),
+            NB_dim=np.array([1, 1]).reshape(-1, 1),
+            PR=np.array(
+                [[-5.01, 2, 0.5], [-1, 5.01, 0.5], [5.01, 2.5, 0.5]]).T,
+            OR=np.array([[0, 0, 0], [-90, 0, 0], [180, 0, 0]]).T,
+            NR_dim=np.array([[10, 10], [10, 10], [10, 10]]).T,
+            PU=np.array([-2, 3, -1.5]).reshape(-1, 1),
+            OU=np.array([0, 0, 0]).reshape(-1, 1),
+            NU_dim=np.array([1, 1]).reshape(-1, 1),
+            K=64,
+            G=10,
             seed=1
         )
 
         if debug:
             print("Starting objective function...")
-                
+
         c.get_beam_matrix()
         c.get_tx_symbol()
         c.get_path_parameters_PWM()
@@ -70,7 +143,8 @@ class ObjectiveFunction:
         grid_size = 0.5     # ~7s
         xgrid = np.arange(-5, 5+grid_size, grid_size)
         ygrid = np.arange(0, 5+grid_size, grid_size)
-        PEB_cell = c.get_PEB_cell(xgrid, ygrid, parallel=True, verbosity=1, print_runtimes=True)
+        PEB_cell = c.get_PEB_cell(
+            xgrid, ygrid, parallel=True, verbosity=1, print_runtimes=True)
         error_grid, PEB_CDF = c.get_PEB_CDF(PEB_cell)
         score = c.get_CDF_threshold(error_grid, PEB_CDF, threshold=0.9)
 
@@ -80,6 +154,5 @@ class ObjectiveFunction:
             print(f'Score: {score: .4f}')
             print("Finished objective function")
             plt.show()
-
 
         return score
--- test_file.py
+++ test_file.py
@@ -1,4 +1,6 @@
 from src.objective_function import ObjectiveFunction
+from src import objective_function
+import numpy as np
 
 def test_obj_get_score():
     obj = ObjectiveFunction()
@@ -7,19 +9,25 @@ def test_obj_get_score():
 
 
 def test_get_placements():
-   from src import objective_function
-   import numpy as np
-
    xgrid = np.arange(-5, 5+0.5, 0.5)
    ygrid = np.arange(0, 5+0.5, 0.5)
 
    slots = objective_function.get_slots_from_side(xgrid, 3)
    print(slots)
 
+def test_get_slots():
+    xgrid = np.arange(-5, 5+0.5, 0.5)
+    ygrid = np.arange(0, 5+0.5, 0.5)
+
+    slots = objective_function.get_slots(xgrid, ygrid, (5, 5, 5, 5), (0.1, 0.1, 0.1, 0.1)) 
+    print(slots)
+
+
 
 def main():
     #test_obj_get_score()
-    test_get_placements()
+    #test_get_placements()
+    test_get_slots()
 
 
 if __name__ == '__main__':
